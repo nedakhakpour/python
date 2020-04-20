c21b01eed31e876dae25b5ea333267d828793967
Neda Khakpour <neda.khakpour@yahoo.com>
2020-04-20 13:40:16 -0500

    Update and rename program_2.py to bigram.py

--- bigram.py
+++ bigram.py
@@ -10,7 +10,7 @@ import pickle
 # CS 4395
 # Homework 3, Program 2
 
-# In this homework you will create bigram and unigram dictionaries for English, French, and Italian using the provided training data 
+# Objective: Create bigram and unigram dictionaries for English, French, and Italian using the provided training data 
 # where the key is the unigram or bigram text and the value is the count of that unigram or bigram in the data. Then for the test data, 
 # calculate probabilities for each language and compare against the true labels.
 
