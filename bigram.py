85faddb4f2ce3ec6361a198e904b826e3619647c
Neda Khakpour <neda.khakpour@yahoo.com>
2020-04-20 13:40:28 -0500

    Update bigram.py

--- bigram.py
+++ bigram.py
@@ -5,11 +5,6 @@ from nltk.util import ngrams
 from nltk.tokenize import word_tokenize
 import pickle
 
-
-# Neda Khakpour
-# CS 4395
-# Homework 3, Program 2
-
 # Objective: Create bigram and unigram dictionaries for English, French, and Italian using the provided training data 
 # where the key is the unigram or bigram text and the value is the count of that unigram or bigram in the data. Then for the test data, 
 # calculate probabilities for each language and compare against the true labels.
